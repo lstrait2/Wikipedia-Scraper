de282f4a4aadcf2ba8039bd50f1fccedd63d70ac
lstrait2 <lstrait2@d9c94b55-221a-4017-ad66-301122ff4019>
2017-03-02 20:10:31 +0000

    refactored

---
+++ lstrait2/Assignment2.1/Assignment2.1/view/movie_views.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, abort, jsonify, request
+import urllib
+
+def construct_movie_blueprint(graph_data):
+
+	movie_blueprint = Blueprint('movie_blueprint', __name__)
+
+	@movie_blueprint .route('/api/movies/<string:movie_name>', methods=['GET'])
+	def get_movie(movie_name):
+		movie = graph_data[1].get(movie_name)
+		if movie is None:
+			#TODO: more detailed error message
+			abort(400)
+		return jsonify(movie), 200
+
+	@movie_blueprint .route('/api/movies/', methods=['GET'])
+	def get_movies():
+		query = request.query_string
+		query = urllib.unquote(query)
+		#TODO: move this method out into util
+		#TODO: recursively split on booleans, keep filtering (make copy each time)
+		if '&' in query:
+			return query.split('&')[0]
+		elif '|' in query:
+			return query.split('|')[0]
+		else:
+			return query
+
+	@movie_blueprint .route('/api/movies/<string:movie_name>', methods=['PUT'])
+	def put_movie(movie_name):
+		movie = graph_data[1].get(movie_name)
+		update = request.json
+		if update is None or movie is None:
+			abort(400)
+		# check all keys are valid before updating anything
+		for key in update.keys():
+			if key not in movie.keys():
+				abort(400)
+		for key in update.keys():
+			movie[key] = update[key]
+		return jsonify(movie), 200
+
+	@movie_blueprint .route('/api/movies/', methods=['POST'])
+	def post_movie():
+		new_movie = request.json
+		if new_movie is None:
+			abort(400)
+		# name key must exist and be unique
+		movie_name = new_movie.get("name")
+		if new_movie is None or movie_name in graph_data[1].keys():
+			abort(400)
+		graph_data[1][movie_name] = {}
+		#TODO: check all keys are valid for type
+		for key in new_movie.keys():
+			graph_data[1][movie_name][key] = new_movie[key]
+		return jsonify(graph_data[1][movie_name]), 201
+
+	@movie_blueprint .route('/api/movies/<string:movie_name>', methods=['DELETE'])
+	def delete_movie(movie_name):
+		movie = graph_data[1].pop(movie_name, None)
+		if movie is None:
+			# TODO: more detailed error message
+			abort(400)
+		return 'Deleting of ' + movie_name + ' was successful!', 200
+
+	return movie_blueprint
+
